2560aec7b77b6539595e877a5c632db706c57f43
Pedro Sabel <pedrosabel08@gmail.com>
2024-08-31 19:11:46 -0300

    .

--- buscaLinhaAJAX.php
+++ buscaLinhaAJAX.php
@@ -2,7 +2,7 @@
 header('Content-Type: application/json');
 
 // Conectar ao banco de dados
-$conn = new mysqli('localhost', 'root', 'improov', 'improov');
+$conn = new mysqli('localhost', 'root', '', 'improov');
 
 // Verificar a conexão
 if ($conn->connect_error) {

1bdda105aa0062a05361d5c82a20c0243c292cdc
Pedro Sabel <pedrosabel08@gmail.com>
2024-09-05 14:21:36 -0300

    .

--- insereFuncao.php
+++ insereFuncao.php
@@ -1,12 +1,7 @@
 <?php
-// Conexão com o banco de dados
-$servername = "localhost";
-$username = "root";
-$password = "improov";
-$dbname = "improov";
 
 // Cria a conexão
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli('192.168.0.202', 'admin', 'admin', 'improov');
 
 // Verifica a conexão
 if ($conn->connect_error) {
@@ -14,28 +9,28 @@ if ($conn->connect_error) {
 }
 
 // Obtém os dados do POST
-$imagem_id = (int)$_POST['imagem_id'];
-$caderno_id = (int)$_POST['caderno_id'];
+$imagem_id = (int) $_POST['imagem_id'];
+$caderno_id = (int) $_POST['caderno_id'];
 $status_caderno = $_POST['status_caderno'];
 $prazo_caderno = $_POST['prazo_caderno'];
 $obs_caderno = $_POST['obs_caderno'];
-$comp_id = (int)$_POST['comp_id'];
+$comp_id = (int) $_POST['comp_id'];
 $status_comp = $_POST['status_comp'];
 $prazo_comp = $_POST['prazo_comp'];
 $obs_comp = $_POST['obs_comp'];
-$model_id = (int)$_POST['model_id'];
+$model_id = (int) $_POST['model_id'];
 $status_modelagem = $_POST['status_modelagem'];
 $prazo_modelagem = $_POST['prazo_modelagem'];
 $obs_modelagem = $_POST['obs_modelagem'];
-$final_id = (int)$_POST['final_id'];
+$final_id = (int) $_POST['final_id'];
 $status_finalizacao = $_POST['status_finalizacao'];
 $prazo_finalizacao = $_POST['prazo_finalizacao'];
 $obs_finalizacao = $_POST['obs_finalizacao'];
-$pos_id = (int)$_POST['pos_id'];
+$pos_id = (int) $_POST['pos_id'];
 $status_pos = $_POST['status_pos'];
 $prazo_pos = $_POST['prazo_pos'];
 $obs_pos = $_POST['obs_pos'];
-$planta_id = (int)$_POST['planta_id'];
+$planta_id = (int) $_POST['planta_id'];
 $status_planta_humanizada = $_POST['status_planta_humanizada'];
 $prazo_planta_humanizada = $_POST['prazo_planta_humanizada'];
 $obs_planta_humanizada = $_POST['obs_planta_humanizada'];
@@ -72,6 +67,8 @@ if ($caderno_funcao_id === null || $comp_funcao_id === null || $modelagem_funcao
     die("Erro: Função não encontrada.");
 }
 
+// Preparar a chamada do stored procedure
+
 $sql = "INSERT INTO funcao_imagem (imagem_id, colaborador_id, funcao_id, prazo, status, observacao) 
         VALUES (?, ?, ?, ?, ?, ?)
         ON DUPLICATE KEY UPDATE
@@ -89,22 +86,22 @@ $conn->begin_transaction();
 
 try {
     // Executa a declaração para cada função
-    $stmt->bind_param("iiisss", $imagem_id, $caderno_id, $caderno_funcao_id, $prazo_caderno, $status_caderno, $obs_caderno);
+    $stmt->bind_param("iiissss", $imagem_id, $caderno_id, $caderno_funcao_id, $prazo_caderno, $status_caderno, $obs_caderno);
     $stmt->execute();
 
-    $stmt->bind_param("iiisss", $imagem_id, $comp_id, $comp_funcao_id, $prazo_comp, $status_comp, $obs_comp);
+    $stmt->bind_param("iiissss", $imagem_id, $comp_id, $comp_funcao_id, $prazo_comp, $status_comp, $obs_comp);
     $stmt->execute();
 
-    $stmt->bind_param("iiisss", $imagem_id, $model_id, $modelagem_funcao_id, $prazo_modelagem, $status_modelagem, $obs_modelagem);
+    $stmt->bind_param("iiissss", $imagem_id, $model_id, $modelagem_funcao_id, $prazo_modelagem, $status_modelagem, $obs_modelagem);
     $stmt->execute();
 
-    $stmt->bind_param("iiisss", $imagem_id, $final_id, $finalizacao_funcao_id, $prazo_finalizacao, $status_finalizacao, $obs_finalizacao);
+    $stmt->bind_param("iiissss", $imagem_id, $final_id, $finalizacao_funcao_id, $prazo_finalizacao, $status_finalizacao, $obs_finalizacao);
     $stmt->execute();
 
-    $stmt->bind_param("iiisss", $imagem_id, $pos_id, $pos_funcao_id, $prazo_pos, $status_pos, $obs_pos);
+    $stmt->bind_param("iiissss", $imagem_id, $pos_id, $pos_funcao_id, $prazo_pos, $status_pos, $obs_pos);
     $stmt->execute();
 
-    $stmt->bind_param("iiisss", $imagem_id, $planta_id, $planta_funcao_id, $prazo_planta_humanizada, $status_planta_humanizada, $obs_planta_humanizada);
+    $stmt->bind_param("iiissss", $imagem_id, $planta_id, $planta_funcao_id, $prazo_planta_humanizada, $status_planta_humanizada, $obs_planta_humanizada);
     $stmt->execute();
 
     // Confirma a transação

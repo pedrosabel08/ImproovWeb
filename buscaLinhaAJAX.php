875e3937dca0aede110ef6a9c743216c571eade5
Pedro Sabel <pedrosabel08@gmail.com>
2024-09-10 16:08:23 -0300

    Observação funcionando corretamente

--- buscaLinhaAJAX.php
+++ buscaLinhaAJAX.php
@@ -19,7 +19,8 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
                     col.idcolaborador AS colaborador_id, 
                     col.nome_colaborador, 
                     fi.prazo, 
-                    fi.status
+                    fi.status,
+                    fi.observacao
                  FROM funcao_imagem fi
                  JOIN colaborador col ON fi.colaborador_id = col.idcolaborador
                  JOIN funcao f ON fi.funcao_id = f.idfuncao

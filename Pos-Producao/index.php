1fe298fd9919be563c9f28f04fca85872a05f886
Pedro Sabel <pedrosabel08@gmail.com>
2024-09-13 13:52:54 -0300

    Lista Pós-Produção melhorada

---
+++ Pos-Producao/index.php
@@ -0,0 +1,231 @@
+<?php
+$conn = new mysqli('192.168.0.202', 'admin', 'admin', 'improov');
+
+if ($conn->connect_error) {
+    die("Falha na conexão: " . $conn->connect_error);
+}
+
+$sql_clientes = "SELECT idcliente, nome_cliente FROM cliente";
+$result_cliente = $conn->query($sql_clientes);
+$clientes = array();
+if ($result_cliente->num_rows > 0) {
+    while ($row = $result_cliente->fetch_assoc()) {
+        $clientes[] = $row;
+    }
+}
+
+$sql_obras = "SELECT idobra, nome_obra FROM obra";
+$result_obra = $conn->query($sql_obras);
+$obras = array();
+if ($result_obra->num_rows > 0) {
+    while ($row = $result_obra->fetch_assoc()) {
+        $obras[] = $row;
+    }
+}
+
+$sql_colaboradores = "SELECT idcolaborador, nome_colaborador FROM colaborador order by nome_colaborador";
+$result_colaboradores = $conn->query($sql_colaboradores);
+$colaboradores = array();
+if ($result_colaboradores->num_rows > 0) {
+    while ($row = $result_colaboradores->fetch_assoc()) {
+        $colaboradores[] = $row;
+    }
+}
+
+$sql_status = "SELECT idstatus, nome_status FROM status_imagem order by idstatus";
+$result_status = $conn->query($sql_status);
+$status_imagens = array();
+if ($result_status->num_rows > 0) {
+    while ($row = $result_status->fetch_assoc()) {
+        $status_imagens[] = $row;
+    }
+}
+<<<<<<< Updated upstream
+=======
+
+$sql_imagens = "SELECT idimagens_cliente_obra, imagem_nome FROM imagens_cliente_obra";
+$result_imagens = $conn->query($sql_imagens);
+$imagens = array();
+if ($result_imagens->num_rows > 0) {
+    while ($row = $result_imagens->fetch_assoc()) {
+        $imagens[] = $row;
+    }
+}
+>>>>>>> Stashed changes
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/stylePos.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastify-js/1.12.0/toastify.min.css">
+
+    <title>Lista Pós-Produção</title>
+</head>
+
+<body>
+    <header>
+<<<<<<< Updated upstream
+        <button id="voltar" onclick="window.location.href='index.html'">Voltar</button>
+=======
+        <button id="voltar" onclick="window.location.href='../index.html'">Voltar</button>
+>>>>>>> Stashed changes
+        <h1>Lista Pós-Produção</h1>
+        <button id="openModalBtn">Inserir render</button>
+    </header>
+
+    <div>
+<<<<<<< Updated upstream
+=======
+        <p>Filtro de tabela</p>
+        <label for="colunaFiltro">Filtrar por:</label>
+        <select id="colunaFiltro">
+            <option value="0">Nome Finalizador</option>
+            <option value="1">Nome Cliente</option>
+            <option value="2">Nome Obra</option>
+            <option value="3">Data</option>
+            <option value="4">Nome Imagem</option>
+            <option value="5">Caminho Pasta</option>
+            <option value="6">Número BG</option>
+            <option value="7">Referências/Caminho</option>
+            <option value="8">Observação</option>
+            <option value="9">Status</option>
+            <option value="10">Revisão</option>
+        </select>
+
+        <input type="text" id="filtro" placeholder="Digite para filtrar" onkeyup="filtrarTabela()">
+
+>>>>>>> Stashed changes
+        <table>
+            <thead>
+                <tr>
+                    <th>Nome Finalizador</th>
+                    <th>Nome Cliente</th>
+                    <th>Nome Obra</th>
+                    <th>Data</th>
+                    <th>Nome imagem</th>
+                    <th>Caminho Pasta</th>
+                    <th>Número BG</th>
+                    <th>Referências/Caminho</th>
+                    <th>Observação</th>
+                    <th>Status</th>
+                    <th>Revisão</th>
+                </tr>
+            </thead>
+            <tbody id="lista-imagens">
+            </tbody>
+        </table>
+    </div>
+
+    <div id="modal" class="modal">
+        <div class="modal-content">
+            <span class="close">&times;</span>
+            <div id="form-inserir">
+                <h2>Formulário de Dados</h2>
+                <form id="formPosProducao">
+                    <div>
+                        <label for="nomeFinalizador">Nome Finalizador</label>
+                        <select name="final_id" id="opcao_finalizador">
+                            <option value="">Selecione um colaborador:</option>
+                            <?php foreach ($colaboradores as $colab): ?>
+                                <option value="<?= htmlspecialchars($colab['idcolaborador']); ?>">
+                                    <?= htmlspecialchars($colab['nome_colaborador']); ?>
+                                </option>
+                            <?php endforeach; ?>
+                        </select>
+                    </div>
+
+                    <div>
+                        <label for="nomeCliente">Nome Cliente</label>
+                        <select name="cliente_id" id="opcao_cliente">
+                            <?php foreach ($clientes as $cliente): ?>
+                                <option value="<?= htmlspecialchars($cliente['idcliente']); ?>">
+                                    <?= htmlspecialchars($cliente['nome_cliente']); ?>
+                                </option>
+                            <?php endforeach; ?>
+                        </select>
+                    </div>
+
+                    <div>
+                        <label for="nomeObra">Nome Obra</label>
+                        <select name="obra_id" id="opcao_obra" onchange="buscarImagens()">
+                            <option value="">Selecione uma obra primeiro:</option>
+                            <?php foreach ($obras as $obra): ?>
+                                <option value="<?= $obra['idobra']; ?>"><?= htmlspecialchars($obra['nome_obra']); ?>
+                                </option>
+                            <?php endforeach; ?>
+                        </select>
+                    </div>
+
+                    <div>
+<<<<<<< Updated upstream
+                        <label for="nomeImagem">Nome Imagem</label>
+                        <select id="nomeImagem" name="imagem_id" required>
+                            <option value="">Selecione uma obra primeiro</option>
+=======
+                        <label for="imagem_id">Nome Imagem</label>
+                        <select id="imagem_id" name="imagem_id" required>
+                            <option value="">Selecione uma obra primeiro</option>
+                            <?php foreach ($imagens as $imagem): ?>
+                                <option value="<?= htmlspecialchars($imagem['idimagens_cliente_obra']); ?>">
+                                    <?= htmlspecialchars($imagem['imagem_nome']); ?>
+                                </option>
+                            <?php endforeach; ?>
+>>>>>>> Stashed changes
+                        </select>
+                    </div>
+
+                    <div>
+                        <label for="caminhoPasta">Caminho Pasta</label>
+                        <input type="text" id="caminhoPasta" name="caminho_pasta">
+                    </div>
+
+                    <div>
+                        <label for="numeroBG">Número BG</label>
+                        <input type="text" id="numeroBG" name="numero_bg">
+                    </div>
+
+                    <div>
+                        <label for="referenciasCaminho">Referências/Caminho</label>
+                        <input type="text" id="referenciasCaminho" name="refs">
+                    </div>
+
+                    <div>
+                        <label for="observacao">Observação</label>
+                        <textarea id="observacao" name="obs" rows="3"></textarea>
+                    </div>
+
+                    <div>
+                        <label for="status">Revisão</label>
+                        <select name="status_id" id="opcao_status">
+                            <?php foreach ($status_imagens as $status): ?>
+                                <option value="<?= htmlspecialchars($status['idstatus']); ?>">
+                                    <?= htmlspecialchars($status['nome_status']); ?>
+                                </option>
+                            <?php endforeach; ?>
+                        </select>
+                    </div>
+
+                    <div>
+                        <label for="status_pos">Status</label>
+                        <input type="checkbox" name="status_pos" id="status_pos" disabled>
+                    </div>
+
+                    <div>
+                        <button type="submit">Enviar</button>
+                    </div>
+                </form>
+            </div>
+
+
+
+            <script src="../script/scriptPos.js"></script>
+            <script src="https://cdn.jsdelivr.net/npm/toastify-js"></script>
+            <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+</body>
+
+</html>
---
+++ script/scriptPos.js
@@ -0,0 +1,295 @@
+<<<<<<< Updated upstream
+=======
+<<<<<<< Updated upstream
+=======
+<<<<<<< Updated upstream
+=======
+>>>>>>> Stashed changes
+// Seleciona o modal, botão e o botão de fechar
+var modal = document.getElementById("modal");
+var openModalBtn = document.getElementById("openModalBtn");
+var closeModal = document.getElementsByClassName("close")[0];
+const formPosProducao = document.getElementById('formPosProducao');
+
+// Abre o modal ao clicar no botão
+openModalBtn.onclick = function () {
+    modal.style.display = "flex";
+}
+
+// Fecha o modal ao clicar no "X"
+closeModal.onclick = function () {
+    modal.style.display = "none";
+}
+
+// Fecha o modal ao clicar fora da área de conteúdo
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+function buscarImagens(obraId = null, imagemSelecionada = null) {
+    obraId = obraId || document.getElementById('opcao_obra').value;
+
+    if (obraId) {
+        // Faz uma requisição AJAX para buscar as imagens da obra selecionada
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', 'buscar_imagens.php?obra_id=' + obraId, true);
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                document.getElementById('nomeImagem').innerHTML = xhr.responseText;
+
+                // Se uma imagem já estiver selecionada, marcá-la como escolhida
+                if (imagemSelecionada) {
+                    var options = document.getElementById('nomeImagem').options;
+                    for (var i = 0; i < options.length; i++) {
+                        if (options[i].text === imagemSelecionada) {
+                            options[i].selected = true;
+                            break;
+                        }
+                    }
+                }
+            }
+        };
+        xhr.send();
+    } else {
+        document.getElementById('nomeImagem').innerHTML = '<option value="">Selecione uma obra primeiro</option>';
+    }
+}
+
+document.getElementById('opcao_obra').addEventListener('change', buscarImagens);
+
+<<<<<<< Updated upstream
+formPosProducao.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    var formData = new FormData(this);
+
+    fetch('inserir_pos_producao.php', {
+        method: 'POST',
+        body: formData
+    })
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('modal').style.display = 'none';
+
+            formPosProducao.reset();
+            buscarImagens();
+
+            Toastify({
+                text: "Dados inseridos com sucesso!",
+                duration: 3000,
+                close: true,
+                gravity: "top",
+                position: "left",
+                backgroundColor: "green",
+                stopOnFocus: true,
+            }).showToast();
+        })
+        .catch(error => console.error('Erro:', error));
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+=======
+
+document.addEventListener("DOMContentLoaded", function () {
+    formPosProducao.addEventListener('submit', function (e) {
+        e.preventDefault();
+
+        var formData = new FormData(this);
+
+        fetch('inserir_pos_producao.php', {
+            method: 'POST',
+            body: formData
+        })
+            .then(response => response.text())
+            .then(data => {
+
+                document.getElementById('modal').style.display = 'none';
+                atualizarTabela();
+                formPosProducao.reset();
+                buscarImagens();
+
+                Toastify({
+                    text: "Dados inseridos com sucesso!",
+                    duration: 3000,
+                    close: true,
+                    gravity: "top",
+                    position: "left",
+                    backgroundColor: "green",
+                    stopOnFocus: true,
+                }).showToast();
+            })
+            .catch(error => console.error('Erro:', error));
+    });
+
+>>>>>>> Stashed changes
+    function atualizarTabela() {
+        fetch('atualizar_tabela.php') // Caminho para o seu script PHP
+            .then(response => response.json())
+            .then(data => {
+                const tabela = document.getElementById('lista-imagens');
+                tabela.innerHTML = ''; // Limpa a tabela atual
+
+                data.forEach(imagem => {
+                    // Cria uma nova linha
+                    const tr = document.createElement('tr');
+                    tr.classList.add('linha-tabela');
+                    tr.setAttribute('data-id', imagem.idpos_producao);
+                    tr.setAttribute('data-obra-id', imagem.idobra); // Adiciona o data-obra-id para uso posterior
+
+                    // Verifica o status_pos e define o texto e a cor de fundo apropriada
+                    let statusTexto = imagem.status_pos == 1 ? 'Não começou' : 'Finalizado';
+                    let statusCor = imagem.status_pos == 1 ? 'red' : 'green';
+
+                    // Adiciona as células à linha
+                    tr.innerHTML = `
+                        <td>${imagem.nome_colaborador}</td>
+                        <td>${imagem.nome_cliente}</td>
+                        <td>${imagem.nome_obra}</td>
+                        <td>${imagem.data_pos}</td>
+                        <td>${imagem.imagem_nome}</td>
+                        <td>${imagem.caminho_pasta}</td>
+                        <td>${imagem.numero_bg}</td>
+                        <td>${imagem.refs}</td>
+                        <td>${imagem.obs}</td>
+                        <td style="background-color: ${statusCor}; color: white;">${statusTexto}</td>
+                        <td>${imagem.nome_status}</td>
+                    `;
+
+                    // Adiciona a linha à tabela
+                    tabela.appendChild(tr);
+                });
+
+                // Adiciona o evento de clique às linhas
+                const linhasTabela = document.querySelectorAll('.linha-tabela');
+                linhasTabela.forEach(linha => {
+                    linha.addEventListener('click', function () {
+                        modal.style.display = "flex";
+                        linhasTabela.forEach(outro => {
+                            outro.classList.remove('selecionada');
+                        });
+
+                        this.classList.add('selecionada');
+
+                        var idImagemSelecionada = this.getAttribute('data-id');
+
+                        $.ajax({
+                            type: "GET",
+                            dataType: "json",
+                            url: "http://localhost:8066/ImproovWeb/Pos-Producao/buscaAJAX.php",
+                            data: { ajid: idImagemSelecionada },
+                            success: function (response) {
+                                if (response.length > 0) {
+                                    setSelectValue('opcao_finalizador', response[0].nome_colaborador);
+                                    setSelectValue('opcao_cliente', response[0].nome_cliente);
+                                    setSelectValue('opcao_obra', response[0].nome_obra);
+<<<<<<< Updated upstream
+
+                                    carregarImagem(response[0].idobra, response[0].imagem_nome);
+
+=======
+                                    setSelectValue('imagem_id', response[0].imagem_nome);
+>>>>>>> Stashed changes
+                                    document.getElementById('caminhoPasta').value = response[0].caminho_pasta;
+                                    document.getElementById('numeroBG').value = response[0].numero_bg;
+                                    document.getElementById('referenciasCaminho').value = response[0].refs;
+                                    document.getElementById('observacao').value = response[0].obs;
+                                    setSelectValue('opcao_status', response[0].status_pos);
+
+<<<<<<< Updated upstream
+                                    // Atualiza o checkbox com base no status_pos
+                                    const checkboxStatusPos = document.getElementById('status_pos');
+                                    checkboxStatusPos.checked = response[0].status_pos == 0; // Marcar o checkbox se status_pos for 0
+                                    checkboxStatusPos.disabled = false; // Habilita o checkbox
+=======
+                                    const checkboxStatusPos = document.getElementById('status_pos');
+                                    checkboxStatusPos.checked = response[0].status_pos == 0;
+                                    checkboxStatusPos.disabled = false;
+>>>>>>> Stashed changes
+                                } else {
+                                    console.log("Nenhum produto encontrado.");
+                                }
+                            },
+                            error: function (jqXHR, textStatus, errorThrown) {
+                                console.error("Erro na requisição AJAX: " + textStatus, errorThrown);
+                            }
+                        });
+                    });
+                });
+            })
+            .catch(error => console.error('Erro ao atualizar a tabela:', error));
+    }
+
+<<<<<<< Updated upstream
+    // Chama a função para atualizar a tabela quando a página carrega
+    atualizarTabela();
+
+    // Desabilita o checkbox quando o formulário é enviado
+    document.getElementById('formPosProducao').addEventListener('submit', function () {
+        document.getElementById('status_pos').disabled = true;
+    });
+    function carregarImagem(obraId, nomeImagem) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', 'buscar_imagens.php?obra_id=' + obraId + '&nome_imagem=' + nomeImagem, true);
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                document.getElementById('nomeImagem').innerHTML = xhr.responseText;
+            }
+        };
+        xhr.send();
+    }
+=======
+    atualizarTabela();
+
+    document.getElementById('formPosProducao').addEventListener('submit', function () {
+        document.getElementById('status_pos').disabled = true;
+    });
+
+>>>>>>> Stashed changes
+
+    function setSelectValue(selectId, valueToSelect) {
+        var selectElement = document.getElementById(selectId);
+        var options = selectElement.options;
+
+        for (var i = 0; i < options.length; i++) {
+            if (options[i].text === valueToSelect) {
+                selectElement.selectedIndex = i;
+                break;
+            }
+        }
+    }
+
+<<<<<<< Updated upstream
+    setInterval(atualizarTabela, 1000);
+});
+=======
+});
+
+function filtrarTabela() {
+    var indiceColuna = document.getElementById("colunaFiltro").value;
+    var filtro = document.getElementById("filtro").value.toLowerCase();
+    var tabela = document.querySelector('#lista-imagens');
+    var linhas = tabela.getElementsByTagName('tr');
+
+    for (var i = 0; i < linhas.length; i++) {
+        var cols = linhas[i].getElementsByTagName('td');
+        var mostraLinha = false;
+
+        if (cols[indiceColuna]) {
+            var valorColuna = cols[indiceColuna].textContent || cols[indiceColuna].innerText;
+            if (valorColuna.toLowerCase().indexOf(filtro) > -1) {
+                mostraLinha = true;
+            }
+        }
+
+        if (mostraLinha) {
+            linhas[i].style.display = '';
+        } else {
+            linhas[i].style.display = 'none';
+        }
+    }
+}
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes

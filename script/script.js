ae4b382e8db8818f25ccdeab67138bdee0fcdf7e
Pedro Sabel <pedrosabel08@gmail.com>
2024-09-14 15:07:44 -0300

    .

--- script/script.js
+++ script/script.js
@@ -17,7 +17,7 @@ document.addEventListener("DOMContentLoaded", function () {
             $.ajax({
                 type: "GET",
                 dataType: "json",
-                url: "http://192.168.0.202:8066/ImproovWeb/buscaLinhaAJAX.php",
+                url: "www.improov.com.br/sistema/buscaLinhaAJAX.php",
                 data: { ajid: idImagemSelecionada },
                 success: function (response) {
                     if (response.nome_imagem) {
@@ -136,7 +136,7 @@ document.addEventListener("DOMContentLoaded", function () {
         $.ajax({
             type: "GET",
             dataType: "json",
-            url: "www.improov.com.br/sistema//buscaNomeImagem.php",
+            url: "www.improov.com.br/sistema/buscaNomeImagem.php",
             data: { ajid: idImagem },
             success: function (response) {
                 if (response.imagem_nome !== undefined && response.status_id !== undefined) {

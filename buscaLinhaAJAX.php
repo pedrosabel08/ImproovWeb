6cbbc2e8046d8be0df75da93d808bbf1a0103f77
Pedro Sabel <pedrosabel08@gmail.com>
2024-09-16 15:55:59 -0300

    .

--- buscaLinhaAJAX.php
+++ buscaLinhaAJAX.php
@@ -1,5 +1,9 @@
 <?php
 header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+
 
 // Conectar ao banco de dados
 $conn = new mysqli('mysql.improov.com.br', 'improov', 'Impr00v', 'improov');

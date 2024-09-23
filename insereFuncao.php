714a497f5925d7a97ebb16194d78ddfcc693b35e
Pedro Sabel <pedrosabel08@gmail.com>
2024-09-23 10:18:14 -0300

    CORS header insereFuncao

--- insereFuncao.php
+++ insereFuncao.php
@@ -1,4 +1,9 @@
 <?php
+header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS"); 
+header("Access-Control-Allow-Headers: Content-Type");
+
 // Conexão com o banco de dados
 $conn = new mysqli('mysql.improov.com.br', 'improov', 'Impr00v', 'improov');
 

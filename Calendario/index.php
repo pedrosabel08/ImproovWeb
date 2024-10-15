610555ab5384c5f476cd8e43c3c6db2e50bab381
Pedro Sabel <pedrosabel08@gmail.com>
2024-10-15 15:45:19 -0300

    .

--- Calendario/index.php
+++ Calendario/index.php
@@ -22,6 +22,8 @@ $conn->close();
     <link href='https://cdn.jsdelivr.net/npm/fullcalendar@5.11.0/main.min.css' rel='stylesheet' />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="icon" href="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTm1Xb7btbNV33nmxv08I1X4u9QTDNIKwrMyw&s" type="image/x-icon">
+
     <link href="style.css" rel="stylesheet">
     <title>Calendário de Entregas</title>
 </head>

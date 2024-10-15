4917994082f5d5812bf19182e4b44f3e45758416
Pedro Sabel <pedrosabel08@gmail.com>
2024-10-15 16:51:03 -0300

    mudança de icones e padrões em todas as páginas

--- Pos-Producao/index.php
+++ Pos-Producao/index.php
@@ -94,13 +94,35 @@ if ($result_imagens->num_rows > 0) {
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastify-js/1.12.0/toastify.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="icon" href="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTm1Xb7btbNV33nmxv08I1X4u9QTDNIKwrMyw&s"
-    type="image/x-icon">
+        type="image/x-icon">
     <title>Lista Pós-Produção</title>
 </head>
 
 <body>
     <header>
-        <button id="voltar" onclick="window.location.href='../inicio.php'">Voltar</button>
+        <button id="menuButton">
+            <i class="fa-solid fa-bars"></i>
+        </button>
+        <div id="menu" class="hidden">
+            <a href="main.php" id="tab-imagens">Visualizar tabela com imagens</a>
+            <a href="Pos-Producao/index.php">Lista Pós-Produção</a>
+
+            <?php if (isset($_SESSION['nivel_acesso']) && ($_SESSION['nivel_acesso'] == 1 || $_SESSION['nivel_acesso'] == 3)): ?>
+                <a href="../infoCliente/index.php" >Informações clientes</a>
+                <a href="Acompanhamento/index.html">Acompanhamentos</a>
+            <?php endif; ?>
+
+            <?php if (isset($_SESSION['nivel_acesso']) && ($_SESSION['nivel_acesso'] == 1 || $_SESSION['nivel_acesso'] == 4)): ?>
+                <a href="../Animacao/index.php">Lista Animação</a>
+            <?php endif; ?>
+
+            <a href="../Metas/index.php">Metas e progresso</a>
+
+            <a id="calendar" class="calendar-btn" href="../Calendario/index.php">
+                <i class="fa-solid fa-calendar-days"></i>
+            </a>
+        </div>
+
         <h1>Lista Pós-Produção</h1>
         <button id="openModalBtn">Inserir render</button>
         <?php if ($_SESSION['nivel_acesso'] == 1): ?>

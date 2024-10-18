f4ac7eb270bd57ca7912bd5167d9d54fd5dead72
Pedro Sabel <pedrosabel08@gmail.com>
2024-10-18 11:53:03 -0300

    filtro por obra com planta

--- getFuncoesPorObra.php
+++ getFuncoesPorObra.php
@@ -29,7 +29,9 @@ $sql = "SELECT
         MAX(CASE WHEN fi.funcao_id = 5 THEN c.nome_colaborador END) AS pos_producao_colaborador,
         MAX(CASE WHEN fi.funcao_id = 5 THEN fi.status END) AS pos_producao_status,
         MAX(CASE WHEN fi.funcao_id = 6 THEN c.nome_colaborador END) AS alteracao_colaborador,
-        MAX(CASE WHEN fi.funcao_id = 6 THEN fi.status END) AS alteracao_status
+        MAX(CASE WHEN fi.funcao_id = 6 THEN fi.status END) AS alteracao_status,
+        MAX(CASE WHEN fi.funcao_id = 7 THEN c.nome_colaborador END) AS planta_colaborador,
+        MAX(CASE WHEN fi.funcao_id = 7 THEN fi.status END) AS planta_status
     FROM funcao_imagem fi
     JOIN imagens_cliente_obra ico ON fi.imagem_id = ico.idimagens_cliente_obra
     JOIN colaborador c ON fi.colaborador_id = c.idcolaborador

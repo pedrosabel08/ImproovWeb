147fa2d903f5939d4011675c3cbde3501b851c9d
Pedro Sabel <pedrosabel08@gmail.com>
2024-11-14 17:00:57 -0300

    tela principal com valores

---
+++ Dashboard/atualizarValores.php
@@ -0,0 +1,50 @@
+<?php
+// Incluir o arquivo de conexão
+include '../conexao.php';
+
+// Criação da conexão
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Checando a conexão
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Query SQL para buscar os dados
+$sql = "SELECT 
+    ROUND(SUM(fi.valor)) AS total_producao, 
+    (SELECT COUNT(DISTINCT idobra) FROM obra WHERE status_obra = 0) AS obras_ativas,
+    ROUND((SELECT SUM(cc.valor) FROM controle_comercial cc)) AS total_orcamento
+FROM 
+    funcao_imagem fi
+JOIN 
+    imagens_cliente_obra ico ON fi.imagem_id = ico.idimagens_cliente_obra
+JOIN 
+    obra o ON o.idobra = ico.obra_id";
+
+// Executar a query
+$result = $conn->query($sql);
+
+$data = array();
+
+// Verificando se há resultados
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        // Formatar os valores em reais
+        $data[] = array(
+            'total_producao' => number_format($row['total_producao'], 0, ',', '.'),
+            'obras_ativas' => $row['obras_ativas'],
+            'total_orcamento' => number_format($row['total_orcamento'], 0, ',', '.')
+        );
+    }
+} else {
+    echo json_encode(array('message' => 'Nenhum dado encontrado'));
+    exit;
+}
+
+// Fechar a conexão
+$conn->close();
+
+// Retornando os dados em formato JSON
+echo json_encode($data);
+?>
--- Dashboard/script.js
+++ Dashboard/script.js
@@ -1,3 +1,26 @@
+function atualizarValores() {
+    fetch('atualizarValores.php')
+        .then(response => response.json())
+        .then(data => {
+            if (data && data.length > 0) {  // Verifica se há dados e se não está vazio
+                const valores = data[0];  // Acessa o primeiro elemento do array
+                
+                // Define os valores nas tags HTML correspondentes
+                document.getElementById('total_orcamentos').textContent = `R$${valores.total_orcamento}`;
+                document.getElementById('total_producao').textContent = `R$${valores.total_producao}`;
+                document.getElementById('obras_ativas').textContent = valores.obras_ativas;
+            } else {
+                console.error("Dados não encontrados");
+            }
+        })
+        .catch(error => {
+            console.error("Erro ao buscar dados:", error);
+        });
+}
+
+// Chama a função para atualizar os valores ao carregar a página
+document.addEventListener("DOMContentLoaded", atualizarValores);
+
 $(document).ready(function () {
     // AJAX para buscar os dados das obras
     $.ajax({

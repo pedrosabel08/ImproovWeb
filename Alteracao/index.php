4032d80ef27e1b284bd6e4f6b357edaad067ea87
Pedro Antonio Sabel <pedrosabel08@gmail.com>
2025-02-06 17:36:07 -0300

    tela de alterações

---
+++ Alteracao/index.php
@@ -0,0 +1,61 @@
+<?php
+session_start();
+$nome_usuario = $_SESSION['nome_usuario'];
+
+include '../conexaoMain.php';
+
+if (!isset($_SESSION['logado']) || $_SESSION['logado'] !== true) {
+    // Se não estiver logado, redirecionar para a página de login
+    header("Location: ../index.html");
+    exit();
+}
+
+$conn = conectarBanco();
+
+$clientes = obterClientes($conn);
+$obras = obterObras($conn);
+$colaboradores = obterColaboradores($conn);
+$status_imagens = obterStatusImagens($conn);
+$funcoes = obterFuncoes($conn);
+
+$conn->close();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/styleSidebar.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
+        integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css">
+    <link rel="icon" href="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTm1Xb7btbNV33nmxv08I1X4u9QTDNIKwrMyw&s"
+        type="image/x-icon">
+    <title>Review</title>
+</head>
+
+<body>
+
+    <?php
+
+    include '../sidebar.php';
+    ?>
+
+    <main>
+
+        <header>CAP_TIV</header>
+
+        <div id="imagens-container"></div>
+
+
+    </main>
+
+    <script src="script.js"></script>
+    <script src="../script/sidebar.js"></script>
+</body>
+
+</html>

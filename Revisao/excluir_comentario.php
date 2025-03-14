4630734635d400f6971da309cc6b9879675d1a09
Pedro Antonio Sabel <pedrosabel08@gmail.com>
2025-03-14 16:57:01 -0300

    Adicionando comentário em cada imagem, editar e excluir.

---
+++ Revisao/excluir_comentario.php
@@ -0,0 +1,19 @@
+<?php
+$data = json_decode(file_get_contents("php://input"));
+$id = $data->id;
+
+include '../conexao.php';
+
+$sql = "DELETE FROM comentarios_imagem WHERE id = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param('i', $id);
+$stmt->execute();
+
+if ($stmt->affected_rows > 0) {
+    echo json_encode(['sucesso' => true]);
+} else {
+    echo json_encode(['sucesso' => false]);
+}
+
+$stmt->close();
+$conn->close();

fc09f07867069040e249fd0b675edaefcdb5dce4
Pedro Antonio Sabel <pedrosabel08@gmail.com>
2025-03-25 13:22:40 -0300

    feat: todas as pessoas conseguem ter acesso as notificações

--- buscar_tarefas.php
+++ buscar_tarefas.php
@@ -3,6 +3,7 @@ session_start();
 include 'conexao.php'; // Inclua a conexão com o banco de dados.
 
 $idusuario = $_SESSION['idusuario']; // ID do usuário logado.
+$idcolaborador = $_SESSION['idcolaborador'];
 
 if (!$idusuario) {
     echo json_encode(['error' => 'Usuário não autenticado']);
@@ -28,7 +29,7 @@ if ($idusuario == 1 || $idusuario == 2) {
             LEFT JOIN imagens_cliente_obra i ON i.idimagens_cliente_obra = f.imagem_id
             WHERE f.funcao_id IN (1, 2, 3, 4, 5, 6, 7, 8, 9) AND f.check_funcao = 0 AND f.status = 'Em aprovação'";
 } elseif ($idusuario == 9) {
-    //Nicolle
+    // Nicolle
     $sql = "SELECT 
         f.idfuncao_imagem,
         f.funcao_id, 
@@ -48,16 +49,41 @@ if ($idusuario == 1 || $idusuario == 2) {
       AND f.check_funcao = 0 
       AND f.status = 'Em aprovação'";
 } else {
-    echo json_encode([]); // Sem tarefas para outros usuários
-    exit;
+    $sql = "SELECT 
+    f.idfuncao_imagem,
+    f.funcao_id, 
+    fun.nome_funcao, 
+    f.status, 
+    f.check_funcao, 
+    f.imagem_id, 
+    i.imagem_nome, 
+    f.colaborador_id, 
+    c.nome_colaborador, 
+    c.telefone
+    FROM funcao_imagem f
+    LEFT JOIN funcao fun ON fun.idfuncao = f.funcao_id
+    LEFT JOIN colaborador c ON c.idcolaborador = f.colaborador_id
+    LEFT JOIN imagens_cliente_obra i ON i.idimagens_cliente_obra = f.imagem_id
+    WHERE f.funcao_id IN (1, 2, 3, 4, 5, 6, 7, 8, 9)
+      AND f.check_funcao = 0 
+      AND f.status = 'Ajuste'
+      AND c.idcolaborador = ?";
+
+    // Preparar a consulta
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("i", $idcolaborador); // Vincula o parâmetro
+    $stmt->execute();
+    $result = $stmt->get_result(); // Obtém o resultado da consulta
 }
 
-// Executar a consulta
-$result = $conn->query($sql);
+// Executar a consulta (apenas para os casos sem bind_param)
+if (!isset($stmt)) {
+    $result = $conn->query($sql);
 
-if ($result === false) {
-    echo json_encode(['error' => 'Erro ao executar a consulta']);
-    exit;
+    if ($result === false) {
+        echo json_encode(['error' => 'Erro ao executar a consulta']);
+        exit;
+    }
 }
 
 // Obter os resultados

db59e596bc9659ba88b5162935e8b019304c8105
Pedro Antonio Sabel <pedrosabel08@gmail.com>
2025-04-17 15:05:59 -0300

    feat: alterações visuais no GANTT

--- GANTT/tabela.php
+++ GANTT/tabela.php
@@ -1,6 +1,7 @@
 <?php
 include '../conexao.php'; // Conexão com o banco
 
+// Query para buscar as imagens
 $sqlImagens = "SELECT img.tipo_imagem, img.imagem_nome
 FROM imagens_cliente_obra img
 JOIN obra o ON img.obra_id = o.idobra
@@ -14,24 +15,24 @@ WHERE o.idobra = 55
 ORDER BY img.tipo_imagem, img.imagem_nome";
 $resultImagens = $conn->query($sqlImagens);
 
+// Query para buscar as etapas
 $sqlEtapas = "SELECT etapa, tipo_imagem, data_inicio, data_fim 
               FROM gantt_prazos 
               WHERE obra_id = 55";
 $resultEtapas = $conn->query($sqlEtapas);
 
-// Determinar intervalo de datas
-$sqlDatas = "SELECT MIN(data_inicio) as primeira_data, MAX(data_fim) as ultima_data FROM gantt_prazos WHERE obra_id = 55 AND data_inicio <> '0000-00-00' AND data_fim <> '0000-00-00'";
+// Query para determinar o intervalo de datas
+$sqlDatas = "SELECT MIN(data_inicio) as primeira_data, MAX(data_fim) as ultima_data 
+             FROM gantt_prazos 
+             WHERE obra_id = 55 AND data_inicio <> '0000-00-00' AND data_fim <> '0000-00-00'";
 $resultDatas = $conn->query($sqlDatas);
 $rowDatas = $resultDatas->fetch_assoc();
-$primeiraData = new DateTime($rowDatas['primeira_data']);
-$ultimaData = new DateTime($rowDatas['ultima_data']);
+$primeiraData = $rowDatas['primeira_data'];
+$ultimaData = $rowDatas['ultima_data'];
 
-date_default_timezone_set('America/Sao_Paulo');
-$periodo = new DatePeriod($primeiraData, new DateInterval('P1D'), $ultimaData->modify('+1 day'));
-$datas = [];
-foreach ($periodo as $data) {
-    $datas[] = $data->format('d/m');
-}
+$sqlObra = "SELECT * FROM obra WHERE idobra = 55";
+$resultObra = $conn->query($sqlObra);
+$rowObra = $resultObra->fetch_assoc();
 
 // Organizar os dados
 $imagens = [];
@@ -44,42 +45,14 @@ while ($row = $resultEtapas->fetch_assoc()) {
     $etapas[$row['tipo_imagem']][] = $row;
 }
 
-// Criar a tabela
-$html = '<table border="1">';
-$html .= '<tr><th>Nome da Imagem</th>';
-foreach ($datas as $data) {
-    $html .= "<th>$data</th>";
-}
-$html .= '</tr>';
+// Retornar os dados em JSON
+header('Content-Type: application/json');
+echo json_encode([
+    'imagens' => $imagens,
+    'etapas' => $etapas,
+    'primeiraData' => $primeiraData,
+    'ultimaData' => $ultimaData,
+    'obra' => $rowObra,
+]);
 
-foreach ($imagens as $tipoImagem => $nomesImagens) {
-    $rowspan = count($nomesImagens);
-    $firstRow = true;
-
-    foreach ($nomesImagens as $imagemNome) {
-        $html .= "<tr><td>$imagemNome</td>";
-
-        if ($firstRow && isset($etapas[$tipoImagem])) {
-            $firstRow = false;
-            $diasUsados = 0;
-            foreach ($etapas[$tipoImagem] as $etapa) {
-                $dataInicio = new DateTime($etapa['data_inicio']);
-                $dataFim = new DateTime($etapa['data_fim']);
-                $colspan = $dataInicio->diff($dataFim)->days + 1;
-                $html .= "<td rowspan='$rowspan' colspan='$colspan' class='" . strtolower(str_replace(['ç', 'ã', 'á', 'é', 'í', 'ó', 'ú'], ['c', 'a', 'a', 'e', 'i', 'o', 'u'], $etapa['etapa'])) . "'>{$etapa['etapa']}</td>";
-                $diasUsados += $colspan;
-            }
-
-            $diasRestantes = count($datas) - $diasUsados;
-            if ($diasRestantes > 0) {
-                $html .= "<td rowspan='$rowspan' colspan='$diasRestantes'></td>";
-            }
-        }
-        $html .= "</tr>";
-    }
-}
-
-$html .= '</table>';
 $conn->close();
-
-echo $html;

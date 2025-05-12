a5d318eb2d175d92f950d10781f8790d2d53f6f4
Pedro Antonio Sabel <pedrosabel08@gmail.com>
2025-05-12 15:33:27 -0300

    feat: mostra somente os renders com 5 dias atrás

--- Render/ajax.php
+++ Render/ajax.php
@@ -7,11 +7,32 @@ if (isset($_GET['action'])) {
     switch ($_GET['action']) {
         case 'getRenders':
             // Buscar todos os renders
-            $sql = "SELECT c.nome_colaborador, idrender_alta, imagem_id, status, data, s.nome_status, i.imagem_nome FROM render_alta r 
-            LEFT JOIN imagens_cliente_obra i on r.imagem_id = i.idimagens_cliente_obra
-            LEFT JOIN colaborador c on r.responsavel_id = c.idcolaborador
-            LEFT JOIN status_imagem s on r.status_id = s.idstatus
-            WHERE r.status != 'Arquivado' ORDER BY data DESC";
+            $sql = "SELECT 
+    c.nome_colaborador, 
+    idrender_alta, 
+    imagem_id, 
+    status, 
+    data, 
+    s.nome_status, 
+    i.imagem_nome 
+FROM 
+    render_alta r
+LEFT JOIN 
+    imagens_cliente_obra i ON r.imagem_id = i.idimagens_cliente_obra
+LEFT JOIN 
+    colaborador c ON r.responsavel_id = c.idcolaborador
+LEFT JOIN 
+    status_imagem s ON r.status_id = s.idstatus
+WHERE 
+    (
+        r.status != 'Arquivado'
+        AND (
+            r.status != 'Finalizado' 
+            OR (r.status = 'Finalizado' AND r.data >= CURDATE() - INTERVAL 5 DAY)
+        )
+    )
+ORDER BY 
+    data DESC";
             $result = $conn->query($sql);
             $renders = [];
 
